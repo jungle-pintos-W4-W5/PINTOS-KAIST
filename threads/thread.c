bb2fc16171ed6c53bd92840d99da29eacae9b8aa
tomk1002 <tkwon999@gmail.com>
2025-11-28 08:43:11 +0000

    feat: update sleep logic + ignore .test_status

--- threads/thread.c
+++ threads/thread.c
@@ -105,10 +105,11 @@ thread_init (void) {
 	};
 	lgdt (&gdt_ds);
 
-	/* Init the globla thread context */
+	/* Init the global thread context */
 	lock_init (&tid_lock);
 	list_init (&ready_list);
 	list_init (&destruction_req);
+	list_init (&sleeping_list);
 
 	/* Set up a thread structure for the running thread. */
 	initial_thread = running_thread ();
